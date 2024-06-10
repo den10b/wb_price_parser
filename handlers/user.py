b0e1c6ab2338aec05d132a8bec09c557be4b2ded
babkin.denis2 <babkin.denis2@wb.ru>
2024-06-10 18:26:00 +0300

    init

--- handlers/user.py
+++ handlers/user.py
@@ -5,6 +5,7 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 
 import bot
+from utils.wb_parser import parse
 
 cmd_router = Router()
 
@@ -12,17 +13,41 @@ cmd_router = Router()
 class States(StatesGroup):
     input_file = State()
 
+
+@cmd_router.message(Command("sstart"))
+async def handler(message: Message, state: FSMContext):
+    res = await parse(["https://www.wildberries.ru/catalog/195932394/detail.aspx",
+                       'https://www.wildberries.ru/catalog/165446363/detail.aspx',
+                       'https://www.wildberries.ru/catalog/197029144/detail.aspx'])
+    for tup in res:
+        await message.answer(f"Артикул: {tup[0]}\n"
+                             f"Цена: {tup[1]}₽")
+    await state.clear()
+
+
 @cmd_router.message(Command("start"))
 async def handler(message: Message, state: FSMContext):
-    await message.answer(f"👋 Добрый день! 👋\n"
-                         f"Жду csv файл с ссылками на товары:")
+    await message.answer(f"👋 Добрый день!\n"
+                         f"Жду ссылки на товары через запятую:")
     await state.clear()
     await state.set_state(States.input_file)
 
 
 @cmd_router.message(States.input_file)
 async def handler(message: Message, state: FSMContext):
-    file_id = message.document.file_id
-    res=await bot.main_bot.get_file(file_id)
-    await message.answer(f"downloaded: {res.model_dump()}")
-
+    all_links = message.text.split(',')
+    wb_links = []
+    ya_links = []
+    mega_links = []
+    for link in all_links:
+        if "www.wildberries.ru" in link:
+            wb_links.append(link)
+        if "megamarket.ru" in link:
+            mega_links.append(link)
+        if "market.yandex.ru" in link:
+            ya_links.append(link)
+    wb_parsed = await parse(wb_links)
+    for tup in wb_parsed:
+        await message.answer(f"Артикул: {tup[0]}\n"
+                             f"Цена: {tup[1]}₽")
+    await state.clear()
---
+++ utils/wb_parser.py
@@ -0,0 +1,26 @@
+import aiohttp
+from bs4 import BeautifulSoup
+
+card_req = "https://card.wb.ru/cards/v2/detail?appType=1&curr=rub&dest=-1252448&spp=30&nm="
+
+
+async def parse(links: list[str]) -> list[tuple[int, str]]:
+    res = []
+    for link in links:
+        nm = 0
+        for link_part in link.split('/'):
+            if link_part.isdigit():
+                nm = link_part
+        card_link = card_req + nm
+        async with aiohttp.ClientSession() as session:
+            async with session.get(card_link) as resp:
+                try:
+                    product = (await resp.json())["data"]["products"][0]
+                    id = int(product["id"])
+                    price = product["sizes"][0]["price"]["total"]
+                    res.append((id, price // 100))
+                    print(f"Цена: {price // 100} ₽")
+                except:
+                    print(f"Ошибочка ((")
+
+    return res
