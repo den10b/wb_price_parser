193f4521d1efb4377662f602eb9089b382c6b992
den10b <den10b@yandex.ru>
2024-06-13 23:06:18 +0300

    ALMOST

--- bot.py
+++ bot.py
@@ -10,11 +10,13 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import config
 import handlers
 from models import User
+from pyvirtualdisplay import Display
 
 storage = MemoryStorage()
 main_bot = Bot(token=config.MAIN_BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
 main_dp = Dispatcher(storage=storage)
-
+display = Display(visible=0, size=(1920, 1080))
+display.start()
 
 async def init_db() -> None:
     docker_url = f"mongodb://{config.DB_USER}:{config.DB_PASS}@db:27017/{config.DB_NAME}?authSource=admin"
@@ -56,4 +58,5 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
+        display.stop()
         logger.error("Bot stopped!")
