b67e350244874759e62fc1a6b7d00086a94ca8dc
babkin.denis2 <babkin.denis2@wb.ru>
2024-06-13 14:38:00 +0300

    Почти Done

--- utils/ya_api.py
+++ utils/ya_api.py
@@ -6,53 +6,58 @@ async def checkToken(business_id: str, oauth_token: str) -> bool:
     Проверка корректности токена.
 
     """
-    url = f'https://api.partner.market.yandex.ru/businesses/{business_id}/offer-mappings'
+    try:
+        url = f'https://api.partner.market.yandex.ru/businesses/{business_id}/offer-mappings'
 
-    headers = {
-        'Authorization': f'Bearer {oauth_token}',
-        'Content-Type': 'application/json'
-    }
+        headers = {
+            'Authorization': f'Bearer {oauth_token}',
+            'Content-Type': 'application/json'
+        }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as response:
-            if response.status == 200:
-                return True
-            elif response.status == 401:
-                return False
-            else:
-                print(f'Неожиданный статус: {response.status} - {await response.text()}')
-                return False
-
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                if response.status == 200:
+                    return True
+                elif response.status == 401:
+                    return False
+                else:
+                    print(f'Неожиданный статус: {response.status} - {await response.text()}')
+                    return False
+    except:
+        return False
 
 async def priceProduct(business_id: str, oauth_token: str, offer_id: str) -> int:
     """
     Получение цены продукта.
 
     """
-    url = f'https://api.partner.market.yandex.ru/businesses/{business_id}/offer-mappings'
+    try:
+        url = f'https://api.partner.market.yandex.ru/businesses/{business_id}/offer-mappings'
 
-    headers = {
-        'Authorization': f'Bearer {oauth_token}',
-        'Content-Type': 'application/json'
-    }
-    data = {
-        "offerIds": [
-            offer_id
-        ]
-    }
+        headers = {
+            'Authorization': f'Bearer {oauth_token}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "offerIds": [
+                offer_id
+            ]
+        }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers, json=data) as response:
-            if response.status == 200:
-                card = await response.json()
-                offer_mappings = card.get('result', {}).get('offerMappings', [])
-                if offer_mappings:
-                    basic_price = offer_mappings[0].get('offer', {}).get('basicPrice', {}).get('value', 0)
-                    return basic_price
-                return 0
-            else:
-                print(f'Error: {response.status} - {await response.text()}')
-                return 0
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers, json=data) as response:
+                if response.status == 200:
+                    card = await response.json()
+                    offer_mappings = card.get('result', {}).get('offerMappings', [])
+                    if offer_mappings:
+                        basic_price = offer_mappings[0].get('offer', {}).get('basicPrice', {}).get('value', 0)
+                        return basic_price
+                    return 0
+                else:
+                    print(f'Error: {response.status} - {await response.text()}')
+                    return 0
+    except:
+        return 0
 
 
 async def setPriceYa(item_id: str, business_id: str, oauth_token: str,
